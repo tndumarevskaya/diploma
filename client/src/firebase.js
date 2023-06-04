68d40aaddd4583c95f4150e580058840feadfc76
tndumarevskaya <dumtanya2009@gmail.com>
2023-06-04 22:22:12 +0300

    a

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,8 +1,8 @@
-import { initializeApp } from 'firebase/app';
-import { getStorage } from 'firebase/storage';
+// import { initializeApp } from 'firebase/app';
+// import { getStorage } from 'firebase/storage';
 
-const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
-const app = initializeApp(firebaseConfig);
-const storage = getStorage(app);
+// const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
+// const app = initializeApp(firebaseConfig);
+// const storage = getStorage(app);
 
-export { storage };
+// export { storage };
