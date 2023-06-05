58cd903b432ed569f51da1cd34bc1e73949c3ce2
tndumarevskaya <dumtanya2009@gmail.com>
2023-06-05 06:02:10 +0300

    a

--- client/src/sockets/chat-socket.js
+++ client/src/sockets/chat-socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io(process.env.REACT_APP_API_URL);
+const socket = io("https://animal-shelter.onrender.com");
 
 export const joinRoom = (chatId) => {
   socket.emit('join', chatId);
